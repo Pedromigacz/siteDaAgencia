b53608343bb636cd8bd0cd69421bae7f95cf6f75
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2021-02-02 20:33:19 -0300

    add home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/Home.module.css'
+
+const Home = () => (
+    <div id="home" className={styles.homeContainer}>
+        <h1 className={styles.mainDisclaimer}>Olá, somos a agência <strong>Pedro Migacz</strong><br/>e nós desenvolvemos sites</h1>
+    </div>
+)
+ 
+export default Home;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import '../styles/global.css'
+import { Home } from '../components'
 
 const IndexPage = () => (
   <div>
-    Hello
+    <Home />
   </div>
 )
 
