8a09c32f722452bbbd55b3fccf4fb38fdb7db0ed
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2021-02-02 22:28:45 -0300

    connect to strapi live server

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,20 @@ module.exports = {
         display: 'swap'
       }
     },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: process.env.GATSBY_STRAPI_URL,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`assets`, `skills`],
+        //If using single types place them in this array.
+        singleTypes: [`contact`, `curriculum`],
+        // Possibility to login with a strapi user, when content types are not publically available (optional).
+        loginData: {
+          identifier: "",
+          password: "",
+        },
+      },
+    },
   ],
 }
